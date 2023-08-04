bcaf5eb00efc341d30f87f789868bb0d9b6e3d5d
unknown <you@example.com>
2023-08-05 04:44:46 +0530

    style

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
-import { Card, Row, Col } from 'react-bootstrap';
+import { Card, Row, Col } from "react-bootstrap";
 import NavBar from "./NavBar";
 
 function Movies() {
@@ -10,20 +10,19 @@ function Movies() {
     const fetchData = async () => {
       const url = "https://hoblist.com/api/movieList";
       const postData = {
-            category: "movies",
-            language: "kannada",
-            genre: "all",
-            sort: "voting"
-          };
+        category: "movies",
+        language: "kannada",
+        genre: "all",
+        sort: "voting",
+      };
       try {
         const response = await axios.post(url, postData);
         let tarr = response.data.result;
-        tarr.sort((a,b)=>{
-          let v1 = a.upVoted?a.upVoted.length:0;
-          let v2 = b.upVoted?b.upVoted.length:0;
-          return v1-v2
-        })
-
+        tarr.sort((a, b) => {
+          let v1 = a.upVoted ? a.upVoted.length : 0;
+          let v2 = b.upVoted ? b.upVoted.length : 0;
+          return v1 - v2;
+        });
         setData(tarr);
       } catch (error) {
         console.log(error);
@@ -32,36 +31,63 @@ function Movies() {
 
     fetchData();
   }, []);
-
   return (
     <>
-    <NavBar/>
-    <div className="my-3 ml-3 flex flex-col w-full">
-    
-      {data ? (
-        data.map((movie) => (
-          <Card style={{ width: '28rem', margin: "auto", marginBottom: "0.9rem" , padding: "0.2rem"}} key={movie._id}>
-            <Row>
-            <Col >
-            <Card.Img src={movie.poster} alt={movie.title} />
-            </Col>
-            <Col >
-            <h4>{movie.title}</h4>
-            <Card.Text><b>Language:</b> {movie.language}</Card.Text>
-            <Card.Text><b>Genre: </b>{movie.genre}</Card.Text>
-            <Card.Text><b>Released Date:</b> {new Date(movie.releasedDate * 1000).toLocaleDateString()}</Card.Text>
-            <Card.Text><b>Director:</b> {movie.director.join(', ')}</Card.Text>
-            <Card.Text><b>Starring:</b> {movie.stars.join(', ')}</Card.Text>
-            <Card.Text><b>Views:</b> {movie.pageViews}</Card.Text>
-            </Col>
-            </Row>
-          </Card>
-        ))
-      ): null}
-    </div>
+      <NavBar />
+      <div>
+        {data
+          ? data.map((movie) => (
+              <Card
+                style={{
+                  width: "28rem",
+                  margin: "auto",
+                  marginBottom: "0.9rem",
+                  padding: "0.2rem",
+                  height:"20%"
+                }}
+                key={movie._id}
+              >
+                <Row>
+                  {/* <Col></Col> */}
+                  <Col className=" h-50">
+                    <Card.Img src={movie.poster} alt={movie.title} className="h-50 p-2"/>
+                  </Col>
+                  <Col className="h-50">
+                    <h4>{movie.title}</h4>
+                    <Card.Text className=" m-0">
+                      <b className=" text-secondary">Genre: </b>
+                      {movie.genre}
+                    </Card.Text>
+                    <Card.Text className=" m-0">
+                      <b className=" text-secondary">Director:</b>{" "}
+                      {movie.director.join(", ")}
+                    </Card.Text>
+                    <Card.Text className=" m-0">
+                      <b className=" text-secondary">Starring:</b>{" "}
+                      {movie.stars.join(", ")}
+                    </Card.Text>
+                    <div className="d-flex">
+                      <Card.Text>
+                        {movie.language} |{" "}
+                        {new Date(
+                          movie.releasedDate * 1000
+                        ).toLocaleDateString()}
+                      </Card.Text>
+                    </div>
+                    <div className="d-flex text-primary">
+                      <Card.Text>
+                        {movie.pageViews} views | voted by {movie.voted.length}{" "}
+                        People
+                      </Card.Text>
+                    </div>
+                  </Col>
+                </Row>
+              </Card>
+            ))
+          : null}
+      </div>
     </>
-
   );
-};
+}
 
-export default Movies
+export default Movies;
